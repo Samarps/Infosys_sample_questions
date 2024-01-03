dc4994e71243bf5165c17f3acb76304594ffdddb
Samarps <samar10pratapsingh@gmail.com>
2024-01-03 22:04:07 +0530

    just started

---
+++ Problem1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int E, N;
+    cout<< "Enter current amount of energy (integer value): E = ";
+    cin>> E;
+    cout<< "Enter no. of exercises (interger value): N = ";
+    cin>> N;
+    int A[N];
+
+    cout<< "Enter amount of energy each exercise drains "<< endl;
+    for (int i = 0; i < N; i++)
+    {
+        cout<<"Exercise "<<i+1<<" :";
+        cin>> A[i];
+    }
+
+    //sorting the energy array in decreasing order
+    for (int i = 0; i < N; i++)
+    {
+        
+    }
+    
+
+
+
+}
